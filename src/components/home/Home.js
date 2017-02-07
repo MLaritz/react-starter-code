ec47a73371ead059892d9ca19977fc5fe8d1a102
Martin Laritz <martin.laritz@gmail.com>
2017-02-06 23:09:44 -0500

    Webpage now loads.

---
+++ src/components/home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div >
+                <h1>React Starter Code</h1>
+            </div>
+        );
+    }
+}
+
+export default Home;
